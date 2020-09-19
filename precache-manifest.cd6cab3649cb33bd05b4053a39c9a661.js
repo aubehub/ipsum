ba0c74e21bd2a8f16a333839353284c484fe7142
aubehub <albalopsanchez@gmail.com>
2020-09-19 18:13:28 -0300

    Updates

---
+++ precache-manifest.cd6cab3649cb33bd05b4053a39c9a661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3447ea9d7d87ec5141475883d391afa3",
+    "url": "/ipsum/index.html"
+  },
+  {
+    "revision": "d371a126629f8f47c546",
+    "url": "/ipsum/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "26cdcb705ffa9aecd6b5",
+    "url": "/ipsum/static/css/main.38c6f183.chunk.css"
+  },
+  {
+    "revision": "d371a126629f8f47c546",
+    "url": "/ipsum/static/js/2.b7359579.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ipsum/static/js/2.b7359579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26cdcb705ffa9aecd6b5",
+    "url": "/ipsum/static/js/main.33217777.chunk.js"
+  },
+  {
+    "revision": "a9f4b3d99520c876138a",
+    "url": "/ipsum/static/js/runtime-main.b3ae2d8c.js"
+  }
+]);
